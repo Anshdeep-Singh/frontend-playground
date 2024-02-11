f39b754e55d55111e928835c35f00101aae5109e
Anshdeep Singh <69358318+Anshdeep-Singh@users.noreply.github.com>
2024-02-11 01:10:00 -0800

    add codeMirror for syntax completion

--- app/page.tsx
+++ app/page.tsx
@@ -1,126 +1,103 @@
-"use client"
-import { useEffect, useState } from "react";
-import ReactDOMServer from "react-dom/server";
-
-interface CodeEditorWithLineNumbersProps {
-  language: string;
-  code: string;
-  onChange: (value: string) => void;
-  onKeyUp: () => void;
-  fontSize: number;
-  textColor: string;
-
-}
-
-const CodeEditorWithLineNumbers: React.FC<CodeEditorWithLineNumbersProps> = ({
-  language,
-  code,
-  onChange,
-  onKeyUp,
-  fontSize,
-  textColor,
-}) => {
-  const lines = code.split("\n");
-
-  return (
-    <div className="relative w-full flex">
-      <div className="w-6 bg-gray-800 text-white p-2 text-right select-none mt-2 pt-4 min-h-[300px]"
-      >
-        {lines.map((_, index) => (
-          <p key={index} className="text-xs"
-            style={{ fontSize: `${fontSize}px`, color: textColor }}
-          >
-            {index + 1}
-          </p>
-        ))}
-      </div>
-      <textarea
-        name={`${language}-code`}
-        id={`${language}-code`}
-        className="w-full min-h-[300px] px-[5px] py-[15px] bg-gray-900 text-white mt-2 rounded-md text-xs border border-gray-700 focus:outline-none focus:border-blue-500"
-        style={{ overflow: 'hidden', fontSize: `${fontSize}px`, resize: 'none', height: 'auto', color: textColor }}
-        value={code}
-        onChange={(e) => onChange(e.target.value)}
-        onKeyUp={onKeyUp}
-      ></textarea>
-    </div>
-  );
-};
+"use client"; 
+import { useCallback, useEffect, useState } from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { html } from "@codemirror/lang-html";
+import { css } from "@codemirror/lang-css";
+import { javascript } from "@codemirror/lang-javascript";
 
 
-export default function Home() {
-  const [htmlCode, setHtmlCode] = useState(`<h1 id="heading" 
-  class="text-4xl text-center font-bold mb-2 shadow-lg">
+const defaultHtmlCode = `<h1 id="heading" class="text-4xl text-center font-bold mb-2 shadow-lg">
   Front end Playground with 
   <span class="text-blue-500">Tailwind CSS</span>
 </h1>
 
-<button 
-  onclick="changeColor(document.getElementById('heading'))" 
-  class="bg-red-500 text-white p-2 rounded">
+<button onclick="changeColor(document.getElementById('heading'))" class="bg-red-500 text-white p-2 rounded">
   Click me
 </button>
 
+<div class="flex justify-center items-center mb-4">
+  <input type="text" class="border border-gray-300 px-4 py-2 rounded-l-lg focus:outline-none" 
+    placeholder="Search...">
+  <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded-r-lg">Search</button>
+</div>
+      
+<div class="grid grid-cols-3 gap-4">
+    <div class="bg-white shadow-lg rounded-lg p-4">
+        <h2 class="text-lg font-bold mb-2">Card 1</h2>
+        <p>Some text here</p>
+    </div>
+    <div class="bg-white shadow-lg rounded-lg p-4">
+        <h2 class="text-lg font-bold mb-2">Card 2</h2>
+        <p>Some text here</p>
+    </div>
+    <div class="bg-white shadow-lg rounded-lg p-4">
+        <h2 class="text-lg font-bold mb-2">Card 3</h2>
+        <p>Some text here</p>
+    </div>
+</div>
+`;
 
-
-
-
-`);
-
-  const [cssCode, setCssCode] = useState(`h1{
-  color:black;
+const defaultCssCode = `h1 {
+  color: black;
   border-radius: 3px;
-  padding:10px;
+  padding: 10px;
 }
 
-button{
-  background:red;
+button {
+  background: red;
 }
+`;
 
-
-
-
-
-
-
-`);
-
-  const [jsCode, setJsCode] = useState(`function changeColor(x){
-  if(x.style.background === 'rgb(176, 139, 130)'){
+const defaultJsCode = `function changeColor(x) {
+  if (x.style.background === 'rgb(176, 139, 130)') {
     x.style.background = 'rgb(241, 250, 238)';
-  }
-  else{
+  } else {
     x.style.background = 'rgb(176, 139, 130)';
   }
 }
+`;
 
+export default function Home() {
 
+  const [htmlCode, setHtmlCode] = useState(
+    typeof localStorage !== 'undefined' && localStorage.getItem("htmlCode") || defaultHtmlCode 
+  );
 
+  const [cssCode, setCssCode] = useState(
+    typeof localStorage !== 'undefined' &&  localStorage.getItem("cssCode") || defaultCssCode
+  );
 
+  const [jsCode, setJsCode] = useState(
+    typeof localStorage !== 'undefined' &&  localStorage.getItem("jsCode") || defaultJsCode
+  );
 
-
-
-
-`);
-
-  const handleOutput = () => {
-    const outputCode = document.getElementById("output") as HTMLIFrameElement | null;
-
-    if (outputCode?.contentDocument && outputCode.contentDocument.body) {
-      const tailwindCSSLink = document.createElement("link");
-      tailwindCSSLink.href = "https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css";
-      tailwindCSSLink.rel = "stylesheet";
-      outputCode.contentDocument.head.appendChild(tailwindCSSLink);
-
-      outputCode.contentDocument.body.innerHTML = ReactDOMServer.renderToStaticMarkup(
-        <div dangerouslySetInnerHTML={{ __html: htmlCode }} />
-      );
-      const styleElement = outputCode.contentDocument.createElement("style");
-      styleElement.innerHTML = cssCode;
-      outputCode.contentDocument.head.appendChild(styleElement);
-      (outputCode.contentWindow as any).eval(jsCode);
+  useEffect(() => {
+    if (typeof localStorage !== 'undefined') {
+      localStorage.setItem("htmlCode", htmlCode);
+      localStorage.setItem("cssCode", cssCode);
+      localStorage.setItem("jsCode", jsCode);
     }
+  }, [htmlCode, cssCode, jsCode]);
+
+  const handleReset = () => {
+    setHtmlCode(defaultHtmlCode);
+    setCssCode(defaultCssCode);
+    setJsCode(defaultJsCode);
   };
+
+const onChangeHTML = useCallback((value: string) => {
+  setHtmlCode(value);
+}, []);
+
+const onChangeCSS = useCallback((value: string) => {
+  setCssCode(value);
+}
+, []);
+
+const onChangeJS = useCallback((value: string) => {
+  setJsCode(value);
+}, []);
+
 
   const [fontSize, setFontSize] = useState(10);
   const [textColor, setTextColor] = useState("#ffffff"); // Initial text color
@@ -143,6 +120,7 @@ button{
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
     <style>
       ${cssCode}
     </style>
@@ -164,52 +142,71 @@ button{
     link.click();
   };
 
+  const srcCode = `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+      <style>
+        ${cssCode}
+      </style>
+      <title>Output</title>
+    </head>
+    <body>
+      ${htmlCode}
+      <script>
+        ${jsCode}
+      </script>
+    </body>
+  </html>`
 
-  useEffect(() => {
-    handleOutput();
-  }, []);
 
   return (
-    <div className="flex flex-col bg-gray-700 py-4 w-full">
-      <div className="left px-10 pt-4 w-full flex flex-row lg:flex-col sm:px-2 sm:pr-[1.5rem]">
-        <div className="w-full mx-2">
+    <div className="flex flex-col bg-gray-700 py-4">
+      <div className="left px-10 pr-8 pt-4 w-full flex flex-row lg:flex-col sm:px-2 sm:pr-[1.5rem]">
+        <div className="w-1/3 mx-2 lg:w-full">
           <div className="flex flex-row">
             <label className="text-white text-xs shadow-lg rounded-lg bg-gray-800 px-4 py-1 w-1/2">HTML</label>
             <button onClick={handleExport}
-              className="w-full rounded-lg bg-gray-800 text-white px-4 py-1 text-xs shadow-lg ml-2 text-center" 
+              className="w-full rounded-lg bg-gray-800 text-white px-4 py-1 text-xs shadow-lg ml-2 text-center"
             >
               Export&nbsp;as&nbsp;HTML&nbsp;&darr;
             </button>
           </div>
-          <CodeEditorWithLineNumbers
-            language="html"
-            code={htmlCode}
-            onChange={(value) => setHtmlCode(value)}
-            onKeyUp={handleOutput}
-            fontSize={fontSize}
-            textColor={textColor}
-          />
+          <CodeMirror
+              className="w-full bg-gray-800 text-white px-4 py-1 text-xs shadow-lg mt-2 rounded-lg"
+              value={htmlCode}
+              height="342px"
+              theme="dark"
+              extensions={[html()]}
+              onChange={onChangeHTML}
+              placeholder={"<h1>Hello, world!</h1>"}
+              style={{ fontSize: `${fontSize}px`, color: textColor }}
+            />
         </div>
-        <div className="w-full mx-2">
+        <div className="w-1/3 mx-2 lg:w-full">
           <div className="flex flex-row">
             <label className="text-white text-xs shadow-lg rounded-lg bg-gray-800 px-4 py-1 w-1/2 lg:mt-2">CSS</label>
-            <input
-              type="color"
-              className="w-full rounded-lg bg-gray-800 text-white px-[4.5rem] py-1 text-xs shadow-lg ml-2 lg:mt-2"
-              value={textColor}
-              onChange={(e) => handleTextColorChange(e.target.value)}
-            />
+            <button onClick={handleReset}
+              className="w-full rounded-lg bg-red-500 text-white px-4 py-1 text-xs shadow-lg ml-2 text-center lg:mt-2"
+            >
+              Reset
+            </button>
           </div>
-          <CodeEditorWithLineNumbers
-            language="css"
-            code={cssCode}
-            onChange={(value) => setCssCode(value)}
-            onKeyUp={handleOutput}
-            fontSize={fontSize}
-            textColor={textColor}
-          />
+          <CodeMirror
+              className="w-full bg-gray-800 text-white px-4 py-1 text-xs shadow-lg mt-2 rounded-lg"
+              value={cssCode}
+              height="342px"
+              theme="dark"
+              extensions={[css()]}
+              placeholder={"h1 { color: red; }"}
+              onChange={onChangeCSS}
+
+              style={{ fontSize: `${fontSize}px`, color: textColor,backgroundColor: 'bg-gray-800' }}
+            />
         </div>
-        <div className="w-full mx-2">
+        <div className="w-1/3 mx-2 lg:w-full">
           <div className="flex flex-row">
             <label className="text-white text-xs shadow-lg rounded-lg bg-gray-800 px-4 py-1 w-1/2 lg:mt-2">JavaScript</label>
             <input
@@ -220,14 +217,16 @@ button{
               min={8}
               max={12} />
           </div>
-          <CodeEditorWithLineNumbers
-            language="js"
-            code={jsCode}
-            onChange={(value) => setJsCode(value)}
-            onKeyUp={() => handleOutput()}
-            fontSize={fontSize}
-            textColor={textColor}
-          />
+          <CodeMirror
+              className="w-full bg-gray-800 text-white px-4 py-1 text-xs shadow-lg mt-2 rounded-lg"
+              value={jsCode}
+              height="342px"
+              theme="dark"
+              extensions={[javascript()]}
+              onChange={onChangeJS}
+              placeholder={"function myFunction() {\n  document.getElementById('demo').innerHTML = 'Hello, world!';\n}"}
+              style={{ fontSize: `${fontSize}px`, color: textColor }}
+            />
         </div>
       </div>
 
@@ -245,7 +244,8 @@ button{
           </div>
           <div id="close"></div>
         </div>
-        <iframe id="output" className="w-[100%] bg-white p-4 min-h-[750px] md:min-h-[450px] ">
+        <iframe id="output" className="w-[100%] bg-white p-4 min-h-[750px] md:min-h-[450px]" 
+        srcDoc={srcCode}>
         </iframe>
       </div>
     </div>
